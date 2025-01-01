5dfa90fe0f877d7ee701d793857fc5255b2baa74
Anthony Bernard <25408995+apb305@users.noreply.github.com>
2025-01-01 15:19:53 -0500

    Updated help section and added demo image

--- src/components/HelpModal.jsx
+++ src/components/HelpModal.jsx
@@ -19,18 +19,32 @@ function Example() {
           <Modal.Title>Instructions</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+          <h4>How to use the app:</h4>
           <ul>
             <li>
-              Please make sure to include the users.csv, classassignments.csv,
-              and class.csv for the Simple File Format (SFF) and users.csv,
-              enrollments.csv, classes.csv, and orgs.csv for OneRoster.
+              This app enables you to update <a href="https://support.hmhco.com/s/article/Simple-File-Format-SFF-Overview-Core-Platforms-ThinkCentral-Holt-McDougal-Online-Ed-Your-Friend-in-Learning" target="_blank" rel="noopener noreferrer">Simple File Format (SFF)</a> or <a href="https://s3.amazonaws.com/downloads.hmlt.hmco.com/Help/ImportMngmt/Administrator/OneRoster_Template_Files/About_OneRoster_Template_Files.htm" target="_blank" rel="noopener noreferrer">OneRoster</a> CSV files and search for users. Its primary purpose is to compare the data in the import files with the existing data on the site.
             </li>
             <br></br>
             <li>
-              You can upload the complete zipped folder containing either the
-              SFF or OneRoster files.
+              The following minimum files are required before uploading:
+              <ul>
+                <li className="mt-2">
+                  <strong>Simple File Format (SFF):</strong> users.csv, classassignments.csv, and class.csv.
+                </li>
+                <li className="mt-2">
+                  <strong>OneRoster:</strong> users.csv, enrollments.csv, classes.csv, and orgs.csv files.
+                </li>
+              </ul>
+            </li>
+            <br></br>
+            <li>
+              You can either click to upload or drag and drop a zipped or extracted folder containing the SFF or OneRoster files.
             </li>
           </ul>
+          <h4>Demo:</h4>
+          <a href="/images/demo.png" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
+            Click here to view demo
+          </a>
         </Modal.Body>
       </Modal>
     </>
